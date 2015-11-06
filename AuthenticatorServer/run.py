4262ae9e44ef30c63d3ba6b358bbada588331ecd
Sam Barani <sambarani897@gmail.com>
2015-11-06 15:17:22 -0800

    Running on Tornado now

--- AuthenticatorServer/run.py
+++ AuthenticatorServer/run.py
@@ -1,4 +1,11 @@
 from app import app
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+from app import app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+	http_server = HTTPServer(WSGIContainer(app))
+	http_server.listen(5000)
+	IOLoop.instance().start()
+	#app.run(host='0.0.0.0')
