fdb61e55ca4fec5df9ecd4ac7d7323c9812b04a7
Sam Barani <sambarani897@gmail.com>
2015-11-06 13:03:17 -0800

    tryign tomerge in

--- AuthenticatorServer/app/routes/home.py
+++ AuthenticatorServer/app/routes/home.py
@@ -1,10 +1,22 @@
 from flask import Blueprint, Response, render_template, session
 import os
+from app import redis_store
 
-home = Blueprint('home', __name__, url_prefix='/home')
+home = Blueprint('home', __name__, url_prefix='')
 
-@home.route('/test')
+@home.route('/')
 def home_page():
 	if session.get('username'):
 		return render_template("login.html")
 	return render_template("register.html")
+
+
+@home.route('/logged_in', methods=['GET'])
+def is_logged_in():
+	print("{} {}".format(session.get('is_logged_in'), session.get('username')))
+	return render_template("loggedin.html")
+
+@home.route('/flush', methods=['GET'])
+def clear_db():
+	redis_store.flushall()
+	return "done"
